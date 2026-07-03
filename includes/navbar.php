ed1f7382af917455f885535a55aafd806c031c0c
Rabi Shrestha <rabishrestha@hotmail.com>
2026-07-03 22:25:45 +0545

    Did small refactoring

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,11 @@
-<nav>
-    <a href="index.php">Home</a>
-    <a href="student.php">Student</a>
-    <a href="college.php">College</a>
-    <a href="gradecalculator.php">Grade Calculator</a>
-    <a href="result.php">Result</a>
-    <a href="about.php">About</a>
-</nav>
+<header>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="student.php">Student</a>
+        <a href="college.php">College</a>
+        <a href="gradecalculator.php">Grade Calculator</a>
+        <a href="result.php">Result</a>
+        <a href="about.php">About</a>
+    </nav>
+</header>
 <hr
